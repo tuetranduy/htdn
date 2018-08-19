c102e97ec30e6a0b259725d190a9f3ad107184d3
tuetranduy <tue1996@gmail.com>
2018-08-19 20:05:30 +0700

    add config to deploy

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,17 +1,30 @@
 module.exports = {
-    apps: [{
-      name: 'htdn-api',
-      script: './api.js'
-    }],
-    deploy: {
-      production: {
-        user: 'ubuntu',
-        host: 'ec2-52-205-185-207.compute-1.amazonaws.com',
-        key: './ssh/htdn-api.ppk',
-        ref: 'origin/master',
-        repo: 'git@github.com:tuetranduy/htdn.git',
-        path: '/home/ubuntu/htdn/production',
-        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
-      }
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: "htdn-api",
+      script: "./api.js"
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-52-205-185-207.compute-1.amazonaws.com",
+      key: "./ssh/htdn-api.ppk",
+      ref: "origin/master",
+      repo: "git@github.com:tuetranduy/htdn.git",
+      path: "/home/ubuntu/htdn/production",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
     }
   }
+};
