2c1a76d6314c5aeec611d7866b216b20a5880627
tuetranduy <tue1996@gmail.com>
2018-08-19 19:58:57 +0700

    update config

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: '52.205.185.207',
-        key: './ssh/htdn-api.pem',
+        host: 'ec2-52-205-185-207.compute-1.amazonaws.com',
+        key: './ssh/htdn-api.ppk',
         ref: 'origin/master',
         repo: 'git@github.com:tuetranduy/htdn.git',
         path: '/home/ubuntu/htdn',
